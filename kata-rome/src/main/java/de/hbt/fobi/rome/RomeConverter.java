193b093a4965d68edcfc49ca88732bcc015aeaba
Christian Binder (HBT) <christian.binder@moovel.com>
2020-03-11 12:28:45 +0100

    rome

--- kata-rome/src/main/java/de/hbt/fobi/rome/RomeConverter.java
+++ kata-rome/src/main/java/de/hbt/fobi/rome/RomeConverter.java
@@ -1,23 +1,63 @@
 package de.hbt.fobi.rome;
 
+import java.util.List;
+import java.util.Map;
+
 public class RomeConverter {
+
+
+  private static final Map<Character, Integer> nero = Map.of(
+    'M', 1000,
+    'D', 500,
+    'C', 100,
+    'L', 50,
+    'X', 10,
+    'V', 5,
+    'I', 1
+  );
+
+  private static final List persephone = List.of(
+    'I',
+    'V',
+    'X',
+    'L',
+    'C',
+    'D',
+    'M');
 
   public static int convertRomeToDe(String rome) {
     int result = 0;
-    if (rome.startsWith("M")) {
-      result += 1000;
-    } else if (rome.startsWith("D")) {
-      result += 500;
-    } else if (rome.startsWith("C")) {
-
-    } else if (rome.startsWith("I")) {
-      result += 1;
+    int currIdx = 0;
+    for (char c : rome.toCharArray()) {
+      int modification;
+      if (nero.containsKey(c)) {
+        modification = nero.get(c);
+      } else {
+        throw new RuntimeException("Wrong Pizza Recipe");
+      }
+      result = applyModification(modification, rome, currIdx, result);
+      currIdx++;
     }
     return result;
   }
 
-  public static String convertDeToRome(int de) {
-    return "II";
+
+  private static int applyModification(int modification, String rome, int currIdx, int result) {
+    if (currIdx == rome.length() - 1) {
+      return result + modification;
+    }
+    int ordinalCurr = persephone.indexOf(rome.charAt(currIdx));
+    int ordinalNext = persephone.indexOf(rome.charAt(currIdx + 1));
+    if (ordinalCurr == -1 || ordinalNext == -1) {
+      throw new RuntimeException("Wrong Pizza Recipe");
+    }
+    if ((ordinalNext - ordinalCurr) > 0 && (ordinalNext - ordinalCurr) <= 2) {
+      return result - modification;
+    } else if (ordinalNext - ordinalCurr > 2) {
+      throw new RuntimeException("Wrong Pizza Recipe");
+    } else {
+      return result + modification;
+    }
   }
 
 }
--- kata-rome/src/test/java/de/hbt/fobi/rome/RomeConverterTest.java
+++ kata-rome/src/test/java/de/hbt/fobi/rome/RomeConverterTest.java
@@ -11,4 +11,42 @@ public class RomeConverterTest {
     Assertions.assertThat(result).isEqualTo(1);
   }
 
+  @Test
+  public void testRomeToDeSubtractionEasy() {
+    int result = RomeConverter.convertRomeToDe("IV");
+    Assertions.assertThat(result).isEqualTo(4);
+  }
+
+  @Test
+  public void testRomeToDeMultipleSubtractions() {
+    int result = RomeConverter.convertRomeToDe("CMIV");
+    Assertions.assertThat(result).isEqualTo(904);
+  }
+
+  @Test
+  public void testRomeToDeCrazySubtractions() {
+    int result = RomeConverter.convertRomeToDe("MMMCMXCIX");
+    Assertions.assertThat(result).isEqualTo(3999);
+  }
+  @Test
+  public void testRomeToDeChainedSubstractions() {
+    int result = RomeConverter.convertRomeToDe("IVL");
+    Assertions.assertThat(result).isEqualTo(44);
+  }
+
+  @Test
+  public void testRomeToDeError() {
+    Throwable thrown = Assertions.catchThrowable(() -> { RomeConverter.convertRomeToDe("IM"); });
+    Assertions.assertThat(thrown).isInstanceOf(Exception.class)
+      .hasMessageContaining("Wrong Pizza Recipe");
+  }
+
+  @Test
+  public void testRomeToDeWrongPizza() {
+    Throwable thrown = Assertions.catchThrowable(() -> { RomeConverter.convertRomeToDe("PIZZA"); });
+    Assertions.assertThat(thrown).isInstanceOf(Exception.class)
+      .hasMessageContaining("Wrong Pizza Recipe");
+  }
+
+
 }
